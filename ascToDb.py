2d8c5b67b4da03619fd812c6090fcd346b9a1a62
juanmicp <juanmicp@hotmail.com>
2017-01-18 18:13:16 +0000

    Fix errors.

--- ascToDb.py
+++ ascToDb.py
@@ -4,6 +4,7 @@
 import sqlite3
 import codecs
 import csv
+import os
 
 dbfile = 'data_berka.db'
 if os.path.exists(dbfile): 
@@ -25,50 +26,52 @@ cur.execute("""	CREATE TABLE district 	(A1 INT,
 					A13 INT,
 					A14 INT,
 					A15 INT,
-					A16 INT);
+					A16 INT)""")
 
-		CREATE TABLE account 	(account_id INT,
+cur.execute("""CREATE TABLE account 	(account_id INT,
 				 	district_id INT,
 				 	frecuency TEXT,
 				 	date TEXT,
-					FOREIGN KEY(district_id) REFERENCES district(A1));
+					FOREIGN KEY(district_id) REFERENCES district(A1))""")
 
-		CREATE TABLE client 	(client_id INT,
+cur.execute("""CREATE TABLE client 	(client_id INT,
 				 	birth_number TEXT,
 				 	district_id INT,
-				 	FOREIGN KEY(district_id) REFERENCES district(A1));
+				 	FOREIGN KEY(district_id) REFERENCES district(A1))""")
 
-		CREATE TABLE disp 	(disp_id INT,
+cur.execute("""CREATE TABLE disp 	(disp_id INT,
 				 	client_id INT,
 				 	account_id INT,
 				 	type TEXT,
 					FOREIGN KEY(client_id) REFERENCES client(client_id),
-					FOREIGN KEY(account_id) REFERENCES account(account_id));
+					FOREIGN KEY(account_id) REFERENCES account(account_id))""")
 
-		CREATE TABLE card 	(card_id INT,
+cur.execute("""CREATE TABLE card 	(card_id INT,
 				 	disp_id INT,
 				 	type TEXT,
 				 	issued TEXT,
-					FOREIGN KEY(disp_id) REFERENCES disp(disp_id));
+					FOREIGN KEY(disp_id) REFERENCES disp(disp_id))""")
 
-		CREATE TABLE loan 	(loan_id INT,
+cur.execute("""CREATE TABLE loan 	(loan_id INT,
 				 	account_id INT,
 				 	date TEXT,
 				 	amount INT,
 					duration INT,
 					payments REAL,
 					status TEXT,
-					FOREIGN KEY(account_id) REFERENCES account(account_id));
+					FOREIGN KEY(account_id) REFERENCES account(account_id))""")
+"""
 
-		CREATE TABLE order 	(order_id INT,
+cur.execute(""CREATE TABLE order 	(order_id INT,
 				 	account_id INT,
 				 	bank_to TEXT,
 				 	account_to TEXT,
 					amount REAL,
 					k_symbol TEXT,
-					FOREIGN KEY(account_id) REFERENCES account(account_id));
+					FOREIGN KEY(account_id) REFERENCES account(account_id))"")
+"""
 
-		CREATE TABLE trans 	(trans_id INT,
+cur.execute("""CREATE TABLE trans 	(trans_id INT,
 				 	account_id INT,
 				 	date TEXT,
 				 	type TEXT,
@@ -78,13 +81,11 @@ cur.execute("""	CREATE TABLE district 	(A1 INT,
 					k_symbol TEXT,
 					bank TEXT,
 					account TEXT,
-					FOREIGN KEY(account_id) REFERENCES account(account_id));
-
-""")
+					FOREIGN KEY(account_id) REFERENCES account(account_id))""")
 
 with open('data_berka/district.asc','rb') as f:
-    data = csv.DictReader(f)
-    to_db = [(i['A1'], i['A2'], i['A3'], i['A4'], i['A5'], i['A6'], i['A7'], i['A8'], i['A9'], i['A10'], i['A11'], i['A12'], i['A13'], i['A14'], i['A15'], i['A16']) for i in data]
+	data = csv.DictReader(f)
+	to_db = [(i['A1'], i['A2'], i['A3'], i['A4'], i['A5'], i['A6'], i['A7'], i['A8'], i['A9'], i['A10'], i['A11'], i['A12'], i['A13'], i['A14'], i['A15'], i['A16']) for i in data]
 
 cur.executemany("INSERT INTO district (A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", to_db)
 con.commit()
