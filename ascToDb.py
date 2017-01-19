3119d4f9985eacbc7bf58c29547da91582f5e736
juanmicp <juanmicp@hotmail.com>
2017-01-19 10:24:16 +0000

    Saved in database finalized.

--- ascToDb.py
+++ ascToDb.py
@@ -3,15 +3,27 @@
 
 import sqlite3
 import codecs
-import csv
 import os
+
+def readAsc (name):
+	f = codecs.open("data_berka/" + name + ".asc", "r", "utf-8")
+	data = []
+	header = True
+	for line in f: 
+		if (not header):
+			row = tuple(line.replace ('"', '').split(";"))
+			if row != []:
+				data.append(row)
+		header = False
+	return data
+
 
 dbfile = 'data_berka.db'
 if os.path.exists(dbfile): 
 	os.remove(dbfile)
 con = sqlite3.connect(dbfile)
 cur = con.cursor()
-cur.execute("""	CREATE TABLE district 	(A1 INT,
+cur.execute("""	CREATE TABLE district 	(A1 INT PRIMARY KEY,
 					A2 TEXT,
 					A3 TEXT,
 					A4 INT,
@@ -28,31 +40,31 @@ cur.execute("""	CREATE TABLE district 	(A1 INT,
 					A15 INT,
 					A16 INT)""")
 
-cur.execute("""CREATE TABLE account 	(account_id INT,
+cur.execute("""CREATE TABLE account 	(account_id INT PRIMARY KEY,
 				 	district_id INT,
 				 	frecuency TEXT,
 				 	date TEXT,
 					FOREIGN KEY(district_id) REFERENCES district(A1))""")
 
-cur.execute("""CREATE TABLE client 	(client_id INT,
+cur.execute("""CREATE TABLE client 	(client_id INT PRIMARY KEY,
 				 	birth_number TEXT,
 				 	district_id INT,
 				 	FOREIGN KEY(district_id) REFERENCES district(A1))""")
 
-cur.execute("""CREATE TABLE disp 	(disp_id INT,
+cur.execute("""CREATE TABLE disp 	(disp_id INT PRIMARY KEY,
 				 	client_id INT,
 				 	account_id INT,
 				 	type TEXT,
 					FOREIGN KEY(client_id) REFERENCES client(client_id),
 					FOREIGN KEY(account_id) REFERENCES account(account_id))""")
 
-cur.execute("""CREATE TABLE card 	(card_id INT,
+cur.execute("""CREATE TABLE card 	(card_id INT PRIMARY KEY,
 				 	disp_id INT,
 				 	type TEXT,
 				 	issued TEXT,
 					FOREIGN KEY(disp_id) REFERENCES disp(disp_id))""")
 
-cur.execute("""CREATE TABLE loan 	(loan_id INT,
+cur.execute("""CREATE TABLE loan 	(loan_id INT PRIMARY KEY,
 				 	account_id INT,
 				 	date TEXT,
 				 	amount INT,
@@ -60,18 +72,8 @@ cur.execute("""CREATE TABLE loan 	(loan_id INT,
 					payments REAL,
 					status TEXT,
 					FOREIGN KEY(account_id) REFERENCES account(account_id))""")
-"""
 
-cur.execute(""CREATE TABLE order 	(order_id INT,
-				 	account_id INT,
-				 	bank_to TEXT,
-				 	account_to TEXT,
-					amount REAL,
-					k_symbol TEXT,
-					FOREIGN KEY(account_id) REFERENCES account(account_id))"")
-"""
-
-cur.execute("""CREATE TABLE trans 	(trans_id INT,
+cur.execute("""CREATE TABLE trans 	(trans_id INT PRIMARY KEY,
 				 	account_id INT,
 				 	date TEXT,
 				 	type TEXT,
@@ -83,13 +85,38 @@ cur.execute("""CREATE TABLE trans 	(trans_id INT,
 					account TEXT,
 					FOREIGN KEY(account_id) REFERENCES account(account_id))""")
 
-with open('data_berka/district.asc','rb') as f:
-	data = csv.DictReader(f)
-	to_db = [(i['A1'], i['A2'], i['A3'], i['A4'], i['A5'], i['A6'], i['A7'], i['A8'], i['A9'], i['A10'], i['A11'], i['A12'], i['A13'], i['A14'], i['A15'], i['A16']) for i in data]
+cur.execute("""CREATE TABLE ord 	(order_id INT PRIMARY KEY,
+				 	account_id INT,
+				 	bank_to TEXT,
+				 	account_to TEXT,
+					amount REAL,
+					k_symbol TEXT,
+					FOREIGN KEY(account_id) REFERENCES account(account_id))""")
 
-cur.executemany("INSERT INTO district (A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11, A12, A13, A14, A15, A16) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", to_db)
+cur.executemany("INSERT INTO district VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", readAsc("district"))
 con.commit()
 
-#Same with others tables...
+#Same with others tables:
+
+cur.executemany("INSERT INTO account VALUES (?, ?, ?, ?)", readAsc("account"))
+con.commit()
+
+cur.executemany("INSERT INTO client VALUES (?, ?, ?)", readAsc("client"))
+con.commit()
+
+cur.executemany("INSERT INTO disp VALUES (?, ?, ?, ?)", readAsc("disp"))
+con.commit()
+
+cur.executemany("INSERT INTO card VALUES (?, ?, ?, ?)", readAsc("card"))
+con.commit()
+
+cur.executemany("INSERT INTO loan VALUES (?, ?, ?, ?, ?, ?, ?)", readAsc("loan"))
+con.commit()
+
+cur.executemany("INSERT INTO ord VALUES (?, ?, ?, ?, ?, ?)", readAsc("order"))
+con.commit()
+
+cur.executemany("INSERT INTO trans VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", readAsc("trans"))
+con.commit()
 
 con.close()
